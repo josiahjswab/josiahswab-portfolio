3d21cdf28aeea2b433efd99a7a79a3ec5686622d
Josiah Swab <josiahjswab@gmail.com>
2019-11-27 22:41:56 -0800

    adds stuff

--- src/components/blocks/ProjectCards.jsx
+++ src/components/blocks/ProjectCards.jsx
@@ -4,12 +4,12 @@ export default function ProjectCards() {
   return (
     <div className="project block-padding-md flex column">
       <h1 className="">&#123; projects &#125;</h1>
+      {/* TODO: Make the title  position: absolute */}
       <div className="spacer-sm" />
 
       <div className="card-container flex">
-        
         {/* card */}
-        <div className="outer-border scale">
+        <div className=" hello outer-border scale">
           <div className=" card-bubble flex shadow2 align-start">
             <img
               className=""
@@ -21,84 +21,14 @@ export default function ProjectCards() {
         {/* end card */}
 
         {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
+          <div className=" card-bubble test flex shadow2 align-start">
             <img
               className=""
               src={require("../../img/berserk.png")}
               alt="Josiah Swab"
             />
           </div>
-        </div>
         {/* end card */}
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-
-        {/* card */}
-        <div className="outer-border scale">
-          <div className=" card-bubble flex shadow2 align-start">
-            <img
-              className=""
-              src={require("../../img/berserk.png")}
-              alt="Josiah Swab"
-            />
-          </div>
-        </div>
-        {/* end card */}
-
       </div>
     </div>
   );
