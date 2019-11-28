eaaf7a15c4e4ab389766097d2f809e696aa91c61
Josiah Swab <josiahjswab@gmail.com>
2019-11-27 20:11:37 -0800

    Adds frames and awesome header with onClick toggling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [toggle, setToggle] = useState("translate3d(0, -100%, 0)");
+
+  const toggleHeader = () => {
+    if (toggle === "translate3d(0, -100%, 0)") {
+      setToggle("translate3d(0, 0, 0)");
+    } else {
+      setToggle("translate3d(0, -100%, 0)");
+    }
+  };
   return (
-
-    <header className="header flex block-padding-md shadow2">
-
-      <img className="shadow" src={require("./josiahswab.jpg")} alt="Josiah Swab" width="200"/>
-      <h1 className=" block-margin">Josiah Swab</h1>
-      <h1 className=" block-margin">Josiah Swab</h1>
-      
+    <header
+      onClick={toggleHeader}
+      style={{ transform: toggle }}
+      className="header flex block-padding-sm shadow2"
+    >
+      <div className="flex align-start">
+        <img
+          className="shadow"
+          src={require("../img/josiahswab.jpg")}
+          alt="Josiah Swab"
+          width="150"
+          onClick={toggleHeader}
+        />
+        <div className="bookmark" onClick={toggleHeader}></div>
+        <h1 className="block-margin"></h1>
+      </div>
+      <ul className="navigation flex  align-end">
+        <li className="block-margin">Home</li>
+        <li className="block-margin">Projects</li>
+        <li className="block-margin">Contact</li>
+      </ul>
     </header>
-    
   );
 }
--- src/components/blocks/ProjectCards.jsx
+++ src/components/blocks/ProjectCards.jsx
@@ -4,53 +4,101 @@ export default function ProjectCards() {
   return (
     <div className="project block-padding-md flex column">
       <h1 className="">&#123; projects &#125;</h1>
-      <div className="spacer-sm"/>
+      <div className="spacer-sm" />
 
       <div className="card-container flex">
+        
         {/* card */}
-        <div className=" scale card-bubble flex shadow">
-          <div className="block-margin card-text rm-top-padding text-align-center flex column">
-            <div className="bookmark">
-              <div className="circle-center"></div>
-            </div>
-            <h2 className="">Berserker Lorem Ipsum</h2>
-            <p className="rm-browser-margin">Lorem Ipsum</p>
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
           </div>
         </div>
-        {/* card end */}
+        {/* end card */}
+
         {/* card */}
-        <div className=" scale card-bubble flex shadow">
-          <div className="block-margin card-text rm-top-padding text-align-center flex column">
-            <div className="bookmark">
-              <div className="circle-center"></div>
-            </div>
-            <h2 className="">Berserker Lorem Ipsum</h2>
-            <p className="rm-browser-margin">Lorem Ipsum</p>
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
           </div>
         </div>
-        {/* card end */}
+        {/* end card */}
         {/* card */}
-        <div className=" scale card-bubble flex shadow">
-          <div className="block-margin card-text rm-top-padding text-align-center flex column">
-            <div className="bookmark">
-              <div className="circle-center"></div>
-            </div>
-            <h2 className="">Berserker Lorem Ipsum</h2>
-            <p className="rm-browser-margin">Lorem Ipsum</p>
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
           </div>
         </div>
-        {/* card end */}
+        {/* end card */}
         {/* card */}
-        <div className=" scale card-bubble flex shadow">
-          <div className="block-margin card-text rm-top-padding text-align-center flex column">
-            <div className="bookmark">
-              <div className="circle-center"></div>
-            </div>
-            <h2 className="">Berserker Lorem Ipsum</h2>
-            <p className="rm-browser-margin">Lorem Ipsum</p>
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
           </div>
         </div>
-        {/* card end */}
+        {/* end card */}
+        {/* card */}
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
+          </div>
+        </div>
+        {/* end card */}
+        {/* card */}
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
+          </div>
+        </div>
+        {/* end card */}
+        {/* card */}
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
+          </div>
+        </div>
+        {/* end card */}
+
+        {/* card */}
+        <div className="outer-border scale">
+          <div className=" card-bubble flex shadow2 align-start">
+            <img
+              className=""
+              src={require("../../img/berserk.png")}
+              alt="Josiah Swab"
+            />
+          </div>
+        </div>
+        {/* end card */}
+
       </div>
     </div>
   );
