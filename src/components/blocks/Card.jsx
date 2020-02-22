9a7cae657a7bd869729180126e9f7c8fae2103f3
Josiah Swab <josiahjswab@gmail.com>
2020-02-22 14:07:09 -0800

    This adds styling and classname changes for the project info

--- src/components/blocks/Card.jsx
+++ src/components/blocks/Card.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Card(props) {
   const {imageSource, altDescription, styles, link} = props
-  console.log(typeof imageSource)
+  const [ hoverTrue, setHoverTrue ] = useState(false);
+
   return(
-    <a href={link} rel='noreferrer noopener' target='_blank'>
-      <div className={`hello scale ${styles}`}>
-        <div className=' card-bubble flex shadow2 align-start'>
-          <img
-            src={imageSource}
-            alt={altDescription}
-          />
+    <>
+      <a href={link} rel='noreferrer noopener' target='_blank' className='card-abs'>
+        <div 
+          className={`hello scale ${styles}`} 
+          onMouseEnter={() => setHoverTrue(true)}
+          onMouseLeave={() => setHoverTrue(false)}
+        >
+          <div className=' card-bubble flex shadow2 align-start'>
+            <img
+              src={imageSource}
+              alt={altDescription}
+            />
+          </div>
         </div>
+      </a>
+      <div 
+        className='card-info' 
+        style={{display: hoverTrue ? 'block' : 'none', zIndex: hoverTrue ? 0 : 2 }}
+      >
+        HELLO DARKNESS MY OLD FRIEND
       </div>
-    </a>
+    </>
   )
 }
